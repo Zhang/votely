e823d148566c8cf11a0e5cb20cc0d4ba6c2e21fc
Zhang <scottzhang235@gmail.com>
2015-09-11 16:21:11 -0700

    this blowwzz

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,10 +75,10 @@ module.exports = function (grunt) {
         files: ['<%= yeoman.app %>/<%= yeoman.scripts %>/**/*.js'],
         tasks: ['htmlbuild:dist', 'newer:copy:app']
       },
-      less: {
-        files: ['<%= yeoman.app %>/<%= yeoman.styles %>/**/*.less'],
-        tasks: ['less:build', 'newer:copy:styles', 'autoprefixer', 'newer:copy:tmp']
-      },
+      // less: {
+      //   files: ['<%= yeoman.app %>/<%= yeoman.styles %>/**/*.less'],
+      //   tasks: ['less:build', 'newer:copy:styles', 'autoprefixer', 'newer:copy:tmp']
+      // },
       gruntfile: {
         files: ['Gruntfile.js'],
         tasks: ['ngconstant:development', 'newer:copy:app']
@@ -501,6 +501,7 @@ module.exports = function (grunt) {
     'ngconstant:development',
     'concurrent:server',
     'autoprefixer',
+    'less:build',
     'newer:copy:app',
     'newer:copy:tmp',
     'htmlbuild:dist'
